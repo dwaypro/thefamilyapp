d8386cd932a073c3c007f6240a5261ef84d79258
Daniel Way <dwaypro@Daniels-MBP.home>
2017-10-26 17:33:00 -0700

    add js home page entertainment feature

---
+++ mobile/FamillyApp/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class famillyapp extends Component{
+  render (){
+    return (
+      <View>
+      	<Text>
+      		WHAT UP!!
+      	</Text>
+      </View>
+    );
+  }
+}
+
